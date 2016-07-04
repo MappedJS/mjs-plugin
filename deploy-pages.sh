04faa402ef754580aa212455b81248f38a449d88
Duve, Michael <mduve@designmail.net>
2016-07-04 10:32:09 +0200

    deploy script updated

--- deploy-pages.sh
+++ deploy-pages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git checkout gh-pages
-git checkout master docs/ reports/ test/ src/ hbs/ img/ dist/ examples coverage/
+git checkout master docs/ reports/ src/ hbs/ img/ dist/ examples/ coverage/
 git commit -m "'Merge' docs, reports and examples from 'master' branch"
 git push origin
 git checkout master
