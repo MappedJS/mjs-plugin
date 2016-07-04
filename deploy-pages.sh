61b3e1a5d6a6ddac9b3ea89be47870bc27c37de8
Duve, Michael <mduve@designmail.net>
2016-07-04 10:24:56 +0200

    gh-pages: initial commit

--- deploy-pages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git checkout gh-pages
-git checkout master docs/ reports/ test/ src/ hbs/ img/ dist/ examples coverage/
-git commit -m "'Merge' docs, reports and examples from 'master' branch"
-git push origin
-git checkout master
